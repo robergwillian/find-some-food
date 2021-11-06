3ffdce98fd0934e07d37a3135b31ec0e70569e6a
Robergwillian <roberg.willian@gmail.com>
2021-11-06 17:03:26 -0300

    Created Home page and reseted styles

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const themes = {
+    colors: {
+        primary: '#6200ee',
+        background: '#000014',
+        text: '#0000B3',
+        success: '#135E01'
+    },
+    fonts: {
+        regular: 'Roboto, sans-serif',
+    }
+}
+
+export default themes
